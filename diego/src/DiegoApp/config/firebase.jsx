12d16e8c5a8f98aab4f7fe5b6dbf621f0fb4abfe
DanCarvajal5 <114154755+DanCarvajal5@users.noreply.github.com>
2025-09-06 14:21:03 +0300

    firebase

---
+++ diego/src/DiegoApp/config/firebase.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjlIUAZaQpbxG4fxS3d5px0YoAwYUSRyw",
+  authDomain: "diego-5b633.firebaseapp.com",
+  projectId: "diego-5b633",
+  storageBucket: "diego-5b633.firebasestorage.app",
+  messagingSenderId: "60207191705",
+  appId: "1:60207191705:web:9cc47116a42b415013ca08",
+  measurementId: "G-R5YGJYPZPT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
