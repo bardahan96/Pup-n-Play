fc115fe79d53d9a15a4d0359a1023db1cbc6bd9b
bardahan96 <bardahan22@icloud.com>
2025-09-10 19:42:48 +0300

    started t oworking ot gloval state for dog

--- diego/src/DiegoApp/app/DiegoApp.jsx
+++ diego/src/DiegoApp/app/DiegoApp.jsx
@@ -2,22 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "../HomePage/HomePage";
 import UserWrapper from "./context/UserWrapper";
 import SignUp from "../Auth/SignUp";
+import { useState } from "react";
 
 
 export default function DiegoApp() {
 
+
     return (
-
+           
         <SignUp/>
-            // <BrowserRouter>
-
-            //     <Routes>
-            //         {/* <Route path="/:user" element={<UserWrapper/>}/> */}
-            //     </Routes>
-            //         <HomePage/>
-
-              
-            // </BrowserRouter>
-
+    )
     
 };
